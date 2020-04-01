4b5c20352b98cd4e62c62a6f1ba2a99bcf02fb21
urospetrovic34 <nonamen212@gmail.com>
2020-04-01 12:27:24 +0200

    Prebacivanje

---
+++ nrt10517.zadatak1.js
@@ -0,0 +1,14 @@
+const mathjs = require('mathjs');
+/*KORISTIO SAM REQUIRE UMESTO IMPORT JER IMPORT JE PRAVIO PROBLEME*/
+let a=mathjs.sqrt(121)
+let b=mathjs.pow(2,5)
+let c=mathjs.cos(3)
+let d=mathjs.evaluate('5+5*5')
+let e=mathjs.complex(5,4)
+let f=mathjs.evaluate('2+100/(176*3)')
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
